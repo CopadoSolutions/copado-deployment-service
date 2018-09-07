82d64e5d45b897d1e71a6e479fa19706269d620b
David Marquez Delgado - Copado <dmarquez@copa.do>
2018-09-07 13:58:13 +0200

    [Test] Run synchronously

--- src/main/java/copado/Application.java
+++ src/main/java/copado/Application.java
@@ -2,13 +2,18 @@ package copado;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableAsync
+@Import(AsyncConfiguration.class)
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 }
+
+
